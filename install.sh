14d38de571fa8e4b8df43f37e6ae9edae44f97f0
hdbjlizhe <hdbjlizhe@hotmail.com>
2022-01-01 21:09:53 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,7 +1,99 @@
+#!/bin/bash
 ############################################
 ##QQ交流群：418353744
 ##QQ线报群：263723430
 ############################################
+color()
+{
+    green='\e[1;32m'
+    res='\e[0m'
+    yellow='\e[1;33m'
+    echo -e ${green}
+    clear
+}
+
+hello(){
+    #!/bin/sh   
+    echo "检查系统..."
+    check_centos=`cat /etc/redhat-release`
+    check=`uname  -a`
+    os=debian
+    cpu=arm64;
+    if [[ $check_centos =~ "CentOS" ]];
+        then os="centos"
+    fi
+    if [[ $check =~ "ubuntu" ]];
+        then os="ubuntu"
+    fi
+    if [[ $check =~ "x86_64" ]]; then
+        cpu="amd64"
+    fi
+    echo "当前系统为：" $os
+    echo "当前架构为：" $cpu
+    sleep 3s
+}
+change()
+{
+    if [[ "$os" == "debian"  &&  "$cpu" == "amd64" ]];then
+        apt-get install wget git tar -y
+        url=https://dl.golang.com.cn/dl/go/go1.17.5.linux-amd64.tar.gz
+        wget $url
+        tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
+    elif [[ "$os" == "centos"  &&  "$cpu" == "amd64"  ]];then
+        yum install wget git openssl openssl-devel gcc make perl tar -y
+        url=https://dl.golang.com.cn/dl/go/go1.17.5.linux-amd64.tar.gz
+        wget $url
+        tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
+    elif [[ $cpu == "arm64" ]];then
+        apt-get install wget git tar -y
+        url=https://dl.golang.com.cn/dl/go/go1.17.5.linux-arm64.tar.gz
+        wget $url
+        tar -C /usr/local -xzf go1.17.5.linux-arm64.tar.gz
+    else
+        exit 1
+    fi
+}
+
+maingo()
+{
+    color
+    GO111MODULE=on
+    GOPROXY=https://goproxy.cn
+    GOROOT=/usr/local/go
+    GOPATH=/usr/local/go/path
+    #GOBIN=/root/go/bin
+    PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+    export GO111MODULE=on
+    export GOPROXY=https://goproxy.cn
+    export GOROOT=/usr/local/go
+    export GOPATH=/usr/local/go/path
+    export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+    command='cat /etc/profile|grep $ GOROOT'
+    if [-n "$command"];then
+        echo -e ${yellow}
+        echo -e ${res}
+        sillyGirl
+        exit 1
+    else
+        hello
+        change
+        #mkdir $HOME/go $HOME/go/src
+        echo "export GOROOT=${GOROOT}">>/etc/profile
+        echo "export GOPATH=${GOPATH}">>/etc/profile
+        echo "export GO111MODULE=on"  >>/etc/profile
+        echo "export GOPROXY=https://goproxy.cn" >>/etc/profile
+        echo "export PATH=${PATH}">>/etc/profile
+        . $HOME/.profile
+        echo -e ${green}
+        sleep 3s
+        echo "Go安装完成"
+        echo -e ${res}
+        sleep 10s
+        sillyGirl
+    fi
+}
+
 sillyGirl()
 {
     color
@@ -44,9 +136,9 @@ sillyGirl()
     git clone https://ghproxy.com/https://github.com/hdbjlizhe/fanli_tb.git;
     git clone https://ghproxy.com/https://github.com/hdbjlizhe/fanli_pdd.git;
     git clone https://ghproxy.com/https://github.com/hdbjlizhe/fanli_vip.git;
-    git clone https://ghproxy.com/https://github.com/hdbjlizhe/yangmao.git;
     git clone https://ghproxy.com/https://github.com/ufuckee/jd_cookie.git;
     git clone https://ghproxy.com/https://github.com/cdle/jd_price.git;
+    git clone https://ghproxy.com/https://github.com/hdbjlizhe/yangmao.git
     #编译
     #if [[ $? == 0 ]]; then 
     #    cd ..;
@@ -55,10 +147,10 @@ sillyGirl()
     #    go build;
     #    echo "编译完成！！！"
     #fi
-    cd ..
+    cd ..;
     sleep 3s
-    wget https://github.com/hdbjlizhe/fanli/releases/download/main/sillyGirl_amd64.tar.gz
-    tar -xzvf sillyGirl_amd64.tar.gz
+    wget https://ghproxy.com/https://github.com/hdbjlizhe/fanli/releases/download/main/sillyGirl_amd64.tar.gz
+    tar -zxvf sillyGirl_amd64.tar.gz
     #将备份的文件拷进sillyGirl文件夹
     sleep 3s
     cd ..
@@ -67,8 +159,7 @@ sillyGirl()
     sleep 3s
     echo "拷贝js文件到sillyGirl目录"
     cp -r replies sillyGirl/develop/;
-    sleep 3s
-    
+    sleep 3s  
     echo "安装完成，开始享用吧!!!"
 }
 
@@ -79,7 +170,6 @@ if [ "$GOROOT" != "" ]; then
     sleep 3s
     sillyGirl
 else
-    echo "当前系统未安装GO环境，请先安装Go环境..."
-    sleep 3s
-    exit 1
+    echo "当前系统未安装GO环境，开始安装Go环境..."
+    maingo
 fi
